84b8ee8f5d372192a7ce944a7dcbb7db6cb66ef7
Ismaellucas-BR <ismaellucas36@gmail.com>
2025-10-05 21:41:11 -0300

    updating link name and making build

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,92 @@
-import Aside from "./components/Aside"
-import ContentMain from "./components/ContentMain"
-import Header from "./components/Header"
+import Aside from "./components/Aside";
+import ContentMain from "./components/ContentMain";
+import Header from "./components/Header";
 
-const posts=[
+const posts = [
   {
-    id:0,
-    author:{
-      avatarUrl:'https://avatars.githubusercontent.com/u/76995730?v=4',
-      name:"Ismael Lucas",
-      role:"Desenvolvedor Front-end"
+    id: 0,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/76995730?v=4",
+      name: "Ismael Lucas",
+      role: "Desenvolvedor Front-end",
     },
-    content:[
-      
-      {type:'paragraph',content:'Fala galeraa 👋'},
-      {type:'paragraph',content:'Acabei de subir mais um projeto no meu portfólio. Projeto do curso de React JS da Rocketseat.🚀'},
-      {type:'link',content:'👉 jane.design/doctorcare'},
-      {type:'link',content:'#novoprojeto'},
-      {type:'link',content:'#reactjs'},
-      {type:'link',content:'#rocketseat'},
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portfólio. Projeto do curso de React JS da Rocketseat.🚀",
+      },
+      { type: "link", content: "👉 developerismaellucas.com" },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#reactjs" },
+      { type: "link", content: "#rocketseat" },
     ],
-    publishedAt:new Date("2024-08-26 08:00:00"),
+    publishedAt: new Date("2024-08-26 08:00:00"),
   },
   {
-    id:1,
-    author:{
-      avatarUrl:'https://avatars.githubusercontent.com/u/101995298?s=100&v=4',
-      name:"Jonas Nunes",
-      role:"Desenvolvedor Front-end"
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/101995298?s=100&v=4",
+      name: "Jonas Nunes",
+      role: "Desenvolvedor Front-end",
     },
-    content:[
-      
-      {type:'paragraph',content:'eai Jovens 👋'},
-      {type:'paragraph',content:'Acabei de subir mais um projeto no meu Github. É um projeto que fiz no NLW Return, evento da Rocketseat.'},
-      {type:'link',content:'#novoprojeto'},
-      {type:'link',content:'#nlw'},
-      {type:'link',content:'#rocketseat'},
+    content: [
+      { type: "paragraph", content: "eai Jovens 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu Github. É um projeto que fiz no NLW Return, evento da Rocketseat.",
+      },
+      { type: "link", content: "#novoprojeto" },
+      { type: "link", content: "#nlw" },
+      { type: "link", content: "#rocketseat" },
     ],
-    publishedAt:new Date("2024-08-16 08:00:00"),
+    publishedAt: new Date("2024-08-16 08:00:00"),
   },
   {
-    id:2,
-    author:{
-      avatarUrl:'https://avatars.githubusercontent.com/u/39921021?s=80&v=4',
-      name:"Victor Vinicius",
-      role:"Desenvolvedor Front-end"
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/39921021?s=80&v=4",
+      name: "Victor Vinicius",
+      role: "Desenvolvedor Front-end",
     },
-    content:[
-      {type:'paragraph',content:'eai reis e rainhas 👋'},
-      {type:'paragraph',content:'Acabei de subir mais um projeto no meu portifa sobre banco de dados.'},
-      {type:'link',content:'👉 aleixo.design/pokemon'},
-      {type:'link',content:'#novoprojetoo'},
-      {type:'link',content:'#rocketseatt'},
+    content: [
+      { type: "paragraph", content: "eai reis e rainhas 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa sobre banco de dados.",
+      },
+      { type: "link", content: "👉 aleixo.design/pokemon" },
+      { type: "link", content: "#novoprojetoo" },
+      { type: "link", content: "#rocketseatt" },
     ],
     publishedAt: new Date("2024-08-17 18:33:00"),
   },
-]
+];
 
 function App() {
-
   return (
     <>
-      <Header/>
+      <Header />
       <div className="grid grid-cols-1 sm:grid-cols-[256px_1fr] max-w-[70rem] my-8 mx-auto py-0 px-4 gap-8 items-start">
-        <Aside/>
+        <Aside />
         <main>
-          {posts.map(post=>{
+          {posts.map((post) => {
             return (
               <ContentMain
-               key={post.id}
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
-            )
+            );
           })}
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,30 +1,37 @@
-import { PencilLine} from "phosphor-react";
-import React from 'react'
+import { PencilLine } from "phosphor-react";
+import Capa from "../assets/capa.jpeg";
 import ImgProfile from "./ImgProfile";
 function Aside() {
   return (
-    <aside className='bg-gray-80 rounded-lg overflow-hidden'>
-        <img 
-            src="https://media.licdn.com/dms/image/v2/C4E16AQHPMVVQkD7FQQ/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1617132334941?e=1729728000&v=beta&t=O93w7TJDrfLdWj27xMWsz-bp7o-mMVHDMh9_Y82Ng04"
-            alt="Foto de varios personagens de star wars juntos"
-            className='w-full h-[72px] object-cover'
+    <aside className="bg-gray-80 rounded-lg overflow-hidden">
+      <img
+        src={Capa}
+        alt="Foto de varios personagens de star wars juntos"
+        className="w-full h-[72px] object-cover"
+      />
+      <div className="flex flex-col items-center font-roboto mb-8">
+        <ImgProfile
+          extraClass="out mt-[calc(0px-1.5rem-6px)]"
+          src="https://avatars.githubusercontent.com/u/76995730?v=4"
         />
-        <div className='flex flex-col items-center font-roboto mb-8'>
-            <ImgProfile extraClass="out mt-[calc(0px-1.5rem-6px)]" src="https://avatars.githubusercontent.com/u/76995730?v=4"/>
-            <strong className='mt-4 text-gray-10 leading-[1.6]'>Ismael Lucas</strong>
-            <span className='text-[0.875rem] text-gray-40 leading-[1.6]'>Desenvolvedor Front-end</span>
-        </div>
+        <strong className="mt-4 text-gray-10 leading-[1.6]">
+          Ismael Lucas
+        </strong>
+        <span className="text-[0.875rem] text-gray-40 leading-[1.6]">
+          Desenvolvedor Front-end
+        </span>
+      </div>
 
-        <footer className='border-t border-gray-60 mt-6 pt-6 px-6 pb-8 hidden'>
-            <a 
-                className='flex justify-center items-center w-full font-roboto bg-transparent text-green-10 border border-green-10 rounded-lg h-[50px] py-0 px-6 font-bold no-underline gap-2 hover:bg-green-50 hover:text-white transition-colors' 
-                href="#">
-                    <PencilLine size={20}/> 
-                    Editar o seu perfil
-            </a>
-        </footer>
+      <footer className="border-t border-gray-60 mt-6 pt-6 px-6 pb-8 hidden">
+        <a
+          className="flex justify-center items-center w-full font-roboto bg-transparent text-green-10 border border-green-10 rounded-lg h-[50px] py-0 px-6 font-bold no-underline gap-2 hover:bg-green-50 hover:text-white transition-colors"
+          href="#">
+          <PencilLine size={20} />
+          Editar o seu perfil
+        </a>
+      </footer>
     </aside>
-  )
+  );
 }
 
-export default Aside
+export default Aside;
